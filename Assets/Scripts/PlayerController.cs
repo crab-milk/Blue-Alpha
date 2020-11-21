a9cb4086bfcd698168aa070754539b57b9f36d76
crab-milk <crab-milk@users.noreply.github.com>
2020-11-21 08:16:39 -0600

    Added standard package

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    PlayerInput playerInput;
+    Rigidbody myRigidBody;
+
+    public float groundSpeed;
+    public float flightSpeed;
+    public float jumpForce;
+
+    //public Transform groundCheck;
+    CapsuleCollider theCollider;
+
+    public bool canFly;
+    public bool isGrounded;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerInput = GetComponent<PlayerInput>();
+        myRigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 moveDirection = new Vector3(horizontal, 0f, vertical) * groundSpeed * Time.deltaTime;
+        transform.Translate(moveDirection);
+
+        //if (Input.GetKeyDown)
+        //isGrounded = Physics.CheckSphere(groundCheck.transform.position, .1f);
+        //if (canFly && isGrounded)
+        //{
+        //    Fly();
+        //}
+    }
+
+    private void Fly()
+    {
+
+    }
+}
---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    public KeyCode toggleFly;
+    public KeyCode flyUp;
+    public KeyCode flyDown;
+}
